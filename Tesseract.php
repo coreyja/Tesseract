6d6760a3723888784b18fd3d65af4b4f80b54fca
Corey Alexander <coreyja@gmail.com>
2013-12-17 10:24:13 -0500

    Initial Commit. Blank PHP extension file.

---
+++ Tesseract.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Tesseract Extension
+ * By: CSSE 374 Team 5
+ *
+ * This extension adds an XML tag which adds the Tesseract to the current page,
+ * with the settings specified by the user.
+ */
+
+
+?>
