41a223662c9e76ed1972f8356c66db46b764cf81
Corey Alexander <coreyja@gmail.com>
2014-02-05 16:54:45 -0500

    up

--- js/main.js
+++ js/main.js
@@ -374,6 +374,8 @@ jQuery(function($) {
 
 	if (coursenumber == 'Tesseract'){
 		getAndDisplayCourses();
+	} else if (typeof isConcept === 'undefined') {
+		getAndDisplayConceptPrereqTree(coursenumber);
 	} else {
 		getAndDisplayPrereqTree(coursenumber);
 	}
